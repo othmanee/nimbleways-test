6f113272e7c4816cd645c38571f194326adca518
Othmane ELAROUI <elaroui.othmane@gmail.com>
2023-10-28 08:12:38 +0100

    First commit

---
+++ src/main/java/ma/nimbleways/nimblewaystest/NimblewaysTestApplication.java
@@ -0,0 +1,13 @@
+package ma.nimbleways.nimblewaystest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimblewaysTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimblewaysTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/nimbleways/nimblewaystest/NimblewaysTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.nimbleways.nimblewaystest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimblewaysTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
